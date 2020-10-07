0b5f2bc4aed64ac9da3b4da93e4566da2897658b
amital <mital.anant@gmail.com>
2020-10-07 13:48:16 +0300

    correcting some grammar in the comments

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,20 +2,20 @@
 # File: guessinggame.sh
 # Description: 
 #              
-# This script prompts the user to guess the number of files in the current dir. If user's
-# guess is correct he/she is congratulated. Otherwise they are informed whether their estimate
-# is high or low. The programme does not terminate till the user does not input the correct 
-# number of files.
+# This script prompts the user to guess the number of files in the current directory. If user's
+# guess is correct he/she is congratulated. Otherwise, the user is informed whether their estimate
+# is high or low. The programme does not terminate till the user's response is correct.
 
 
 function guessfilecount {
 
     # Calculate number of files in the current working directory
     filecount=$(ls -la|grep "^-"|wc -l)
+
     guess=-1
     while [[ $guess -ne filecount ]]
     do
-        echo "Guess how many files are in current directory ? "
+        echo "Guess how many files are in the current directory ? "
         read guess
         if [[ $guess -eq filecount ]]
             then
